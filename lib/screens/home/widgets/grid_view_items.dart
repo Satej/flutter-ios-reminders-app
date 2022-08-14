46fc589028afd1bf337e574b5acd0f3011c4ab6a
gecko <gecko>
2022-08-14 19:08:06 +0530

    category click

--- lib/screens/home/widgets/grid_view_items.dart
+++ lib/screens/home/widgets/grid_view_items.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ios_reminders/screens/view_list/view_list_by_category_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../../../models/category/category.dart';
@@ -24,38 +25,50 @@ class GridViewItems extends StatelessWidget {
       padding: const EdgeInsets.all(10),
       children: categories
           .map(
-            (category) => Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: const Color(0xFF1A191D),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    category.icon,
-                    Text(
-                      getCategoryCount(
-                        id: category.id,
-                        allReminders: allReminders,
-                      ).toString(),
-                      style: Theme.of(context).textTheme.headline6,
+            (category) => InkWell(
+              onTap: getCategoryCount(id: category.id, allReminders: allReminders) > 0
+                ? () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) =>
+                        ViewListByCategoryScreen(category: category,)
+                      ),
+                    );
+                  }
+                : null,
+              child: Ink(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFF1A191D),
+                ),
+                child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            category.icon,
+                            Text(
+                              getCategoryCount(
+                                id: category.id,
+                                allReminders: allReminders,
+                              ).toString(),
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                          ],
+                        ),
+                        Text(
+                          category.name,
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      ],
                     ),
-                  ],
                 ),
-                Text(
-                  category.name,
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
       ).toList(),
     );
   }
